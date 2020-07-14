a7a525b969943a6f3a934c400d4f3fcd9300e887
Leonardo Dominguez <haaroldd@icloud.com>
2020-07-14 00:20:25 -0400

    Add Weather Data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,9 +1,9 @@
 const request = require('request')
+
 const forecast = (latitude, longitude, callback) => {
 
     const urlweather = 'http://api.weatherstack.com/current?access_key=d55315028ce77b45f81345f0e18f59d7&query=' + latitude + ',' + longitude + '&units=m'
         
-
     request({ url: urlweather, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
@@ -12,7 +12,7 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location', undefined)
 
         } else {
-            callback(undefined, "Weather in " + body.location.region + "." + body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out. It feels like " + body.current.feelslike + " degress out.")
+            callback(undefined, "Weather in " + body.location.region + "." + body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out. It feels like " + body.current.feelslike + " degress out. Humidity is " + body.current.humidity)
         }
      })
 
